e44b24436432b65eed4caa899207c7cac624ec52
Amya Moore <bookworm7572@gmail.com>
2024-10-19 19:16:08 -0600

    Added Image
    IDK why image routing is not working, so manually inputted image paths

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,25 @@ import Button from "@mui/material/Button";
 import AppleIcon from "@mui/icons-material/Apple";
 import GoogleIcon from "@mui/icons-material/Google";
 import Navigation from "./components/navigation";
-import Image from "next/legacy/image";
+import Image from "next/image";
+import homeScreen from "./public/assets/mobile-home-screen.svg";
+import searchScreen from './public/assets/mobile-search-screen.svg';
+import filterByScreen from './public/assets/mobile-filter-1-screen.svg';
+import filterTagsScreen from './public/assets/mobile-filter-2-screen.svg';
 import Footer from "./components/footer";
 
 export default function Home() {
 	return (
 		<div className="page-container sm:col-span-4 md:col-span-6 lg:col-span-12 xl:col-span-12">
 			<Navigation current={"home"} />
+
 			<section
 				id="header"
-				className="md:grid md:grid-cols-2 md:gap-4 md:items-center"
+				className="md:grid md:grid-cols-2 md:gap-4 md:items-center md:my-[3rem]"
 			>
-				<div className="header">
-					<h1>
-						All your notes, <span className="text-highlight">effortlessly</span>{" "}
+				<div className="header mb-8 lg:px-[5rem]">
+					<h1 className="leading-none">
+						All your notes, <br /> <span className="text-highlight">effortlessly</span>{" "}
 						managed
 					</h1>
 					<p className="py-4">
@@ -45,71 +50,59 @@ export default function Home() {
 						</Button>
 					</div>
 				</div>
-				<div className="image">
+				<div className="image flex flex-row justify-center">
 					<Image
-						src="/assets/mobile-home-screen.png"
+						src={homeScreen}
 						alt="Screenshot of the Notepad app's home screen, featuring a clean, minimalist interface with a quick search bar at the top, a list of recent notes, and options for creating new notes"
-						width={100}
-						height={100}
-						layout="responsive"
-						className="w-full"
+						className="lg:w-[38%] md:w-[70%]"
 					/>
 				</div>
 			</section>
 
-			<section id="features" className="text-center my-4 py-4">
-				<h2 className="mb-4">Designed for Speed & Simplicity</h2>
+			<section id="features" className="text-center my-[4rem]">
+				<h2>Designed for <span className="text-highlight">Speed & Simplicity</span></h2>
 				<div className="features md:grid md:grid-cols-3 md:gap-4">
-					<div className="feature">
-						<div className="image">
+					<div className="feature my-[4rem]">
+						<div className="image flex flex-row justify-center mb-6">
 							<Image
-								src="/public/mobile-search-screen.png"
+								src={searchScreen}
 								alt="Screenshot of the Notepad app's instant search bar displaying search results based on relevant keywords"
-								width={800}
-								height={500}
-								layout="responsive"
 							/>
 						</div>
-						<div className="content">
+						<div className="content flex flex-col justify-center items-center">
 							<h3 className="mb-2">Instant Search</h3>
-							<p>
+							<p className="">
 								Quickly find notes as you type with your responsive search bar,
 								ensuring you never lose track of important information.
 							</p>
 						</div>
 					</div>
-					<div className="feature">
-						<div className="image">
+					<div className="feature my-[4rem]">
+						<div className="image flex flex-row justify-center mb-6">
 							<Image
-								src="/public/mobile-filter-1--screen.png"
+								src={filterByScreen}
 								alt="Screenshot of the Notepad app's dropdown menu, allowing users to filter notes by time or date, with options such as 'Time' or 'Date'"
-								width={800}
-								height={500}
-								layout="responsive"
 							/>
 						</div>
-						<div className="content">
+						<div className="content flex flex-col justify-center items-center">
 							<h3 className="mb-2">Filter by Time or Date</h3>
-							<p>
+							<p className="">
 								Easily organise your notes by filtering the based on the time or
 								date they were created or modified, giving you quick access to
 								relevant content.
 							</p>
 						</div>
 					</div>
-					<div className="feature">
-						<div className="image">
+					<div className="feature my-[4rem]">
+						<div className="image flex flex-row justify-center mb-6">
 							<Image
-								src="/public/mobile-filter-2-screen.png"
+								src={filterTagsScreen}
 								alt="Screenshot of the Notepad app's customisable tags, allowing users organise and categorise notes by tags"
-								width={800}
-								height={500}
-								layout="responsive"
 							/>
 						</div>
-						<div className="content">
+						<div className="content flex flex-col justify-center items-center">
 							<h3 className="mb-2">Tagging System</h3>
-							<p>
+							<p className="">
 								Add customisable tags to your notes for efficient organisation,
 								making it simple to categorise and retrieve notes based on
 								topics or themes.
@@ -121,7 +114,7 @@ export default function Home() {
 
 			<section id="call-to-action" className="text-center px-4">
 				<div className="content bg-[var(--dark-grey)] rounded-xl p-12 mb-32">
-					<h1>Try notepad today</h1>
+					<h1 className="mb-2">Try notepad today</h1>
 					<p>
 						Start simplifying your life today with Notepad - where efficiency
 						meets ease.
